9b351047777740fb6aa93b48fd45ed6b9cbb599d
Riajul-56 <riajulhasansiam@gmail.com>
2026-01-07 01:29:46 +0600

    Add HeroSection component with spotlight animation and update layout structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import HeroSection from "@/components/HeroSection";
+
 export default function Home() {
   return (
-    <h1 className="text-2xl text-center mt-20 font-bold
-   ">Riajul Hasan</h1>
+    <main className="min-h-screen bg-black/[0.95] antialiased bg-grid-white/[0.02]">
+      <h1 className="text-2xl text-center mt-20 font-bold">Riajul Hasan</h1>
+      <HeroSection />
+    </main>
   );
 }
