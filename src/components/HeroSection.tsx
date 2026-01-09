a83d42e2391d5f36efb71f298d3c4f01e78274ab
Riajul-56 <riajulhasansiam@gmail.com>
2026-01-10 02:45:18 +0600

    feat: adjust Spotlight position and simplify TypewriterEffectSmooth component usage

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -26,16 +26,13 @@ const HeroSection = () => {
   return (
     <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
       <Spotlight
-        className="-top-40 left-0 md:-top-20 lg:left-150 md:left-60"
+        className="-top-10 left-10 md:-top-20 lg:left-150 md:left-60"
         fill="white"
       />
 
       <div className="p-4 relative z-10 w-full text-center">
         <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
-          <TypewriterEffectSmooth
-            words={words}
-            className="justify-center"
-          />
+          <TypewriterEffectSmooth words={words} className="justify-center" />
         </h1>
 
         <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
